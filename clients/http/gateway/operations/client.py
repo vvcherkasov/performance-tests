b8e6a70db333a17d5a768094329cdda93e9c1a9f
vvcherkasov <cherkasov612@yandex.ru>
2026-06-22 18:39:49 +0300

    Практика реализации HTTP API клиентов: /api/v1/operations

---
+++ clients/http/gateway/operations/client.py
@@ -0,0 +1,168 @@
+from typing import TypedDict
+
+from httpx import Response, QueryParams
+
+from clients.http.client import HTTPClient
+
+class GetOperationsQueryDict(TypedDict):
+    """
+    Структура query параметров запроса для получения списка операций по счёту.
+    """
+    accountId: str
+
+class GetOperationsSummaryQueryDict(TypedDict):
+    """
+    Структура query параметров запроса для получения статистики по операциям счёта.
+    """
+    accountId: str
+
+class MakeOperationRequestDict(TypedDict):
+    """
+    Базовая структура тела запроса для создания финансовой операции.
+    """
+    status: str
+    amount: float
+    cardId: str
+    accountId: str
+
+class MakeFeeOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции комиссии.
+    """
+    pass
+
+class MakeTopUpOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции пополнения.
+    """
+    pass
+
+class MakeCashbackOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции кэшбэка.
+    """
+    pass
+
+class MakeTransferOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции перевода.
+    """
+    pass
+
+class MakePurchaseOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции покупки.
+    """
+    category: str
+
+class MakeBillPaymentOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции оплаты по счету.
+    """
+    pass
+
+class MakeCashWithdrawalOperationRequestDict(MakeOperationRequestDict):
+    """
+    Структура данных для создания операции снятия наличных денег.
+    """
+    pass
+
+class OperationsGatewayHTTPClient(HTTPClient):
+    def get_operation_api(self, operation_id: str) -> Response:
+        """
+        Получение информации об операции по operation_id.
+
+        :param operation_id: Идентификатор операции.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get(f'/api/v1/operations/{operation_id}')
+
+    def get_operation_receipt_api(self, operation_id: str) -> Response:
+        """
+        Получение чека по операции по operation_id
+
+        :param operation_id: Идентификатор операции.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get(f'/api/v1/operations/operation-receipt/{operation_id}')
+
+    def get_operations_api(self, query: GetOperationsQueryDict) -> Response:
+        """
+        Получение списка операций для определенного счета
+
+        :param query: Словарь с параметрами запроса
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get('/api/v1/operations', params=QueryParams(**query))
+
+    def get_operations_summary_api(self, query: GetOperationsSummaryQueryDict) -> Response:
+        """
+        Получение статистики по операциям для определенного счета
+
+        :param query: Словарь с параметрами запроса
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get('/api/v1/operations/operations-summary',  params=QueryParams(**query))
+
+    def make_fee_operation_api(self, request: MakeFeeOperationRequestDict) -> Response:
+        """
+        Создание операции комиссии.
+
+        :param request: Словарь с данными для операции комиссии.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-fee-operation', json=request)
+
+    def make_top_up_operation_api(self, request: MakeTopUpOperationRequestDict) -> Response:
+        """
+        Создание операции пополнения.
+
+        :param request: Словарь с данными для операции пополнения.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-top-up-operation', json=request)
+
+    def make_cashback_operation_api(self, request: MakeCashbackOperationRequestDict) -> Response:
+        """
+        Создание операции кэшбэка.
+
+        :param request: Словарь с данными для операции кэшбэка.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-cashback-operation', json=request)
+
+    def make_transfer_operation_api(self, request: MakeTransferOperationRequestDict) -> Response:
+        """
+        Создание операции перевода.
+
+        :param request: Словарь с данными для операции перевода.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-transfer-operation', json=request)
+
+    def make_purchase_operation_api(self, request: MakePurchaseOperationRequestDict) -> Response:
+        """
+        Создание операции покупки.
+
+        :param request: Словарь с данными для операции покупки.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-purchase-operation', json=request)
+
+    def make_bill_payment_operation_api(self, request: MakeBillPaymentOperationRequestDict) -> Response:
+        """
+        Создание операции оплаты по счету.
+
+        :param request: Словарь с данными для операции оплаты по счету.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-bill-payment-operation', json=request)
+
+    def make_cash_withdrawal_operation_api(self, request: MakeCashWithdrawalOperationRequestDict) -> Response:
+        """
+        Создание операции снятия наличных денег.
+
+        :param request: Словарь с данными для операции снятия наличных денег.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/operations/make-cash-withdrawal-operation', json=request)
